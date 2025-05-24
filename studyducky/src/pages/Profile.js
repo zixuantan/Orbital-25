1bf6a1ef5e881225df6dc22f4ac8ab1793916af5
lorelier <joliengxuan@gmail.com>
2025-05-25 02:17:58 +0800

    did the account side partially but format not correct

--- studyducky/src/pages/Profile.js
+++ studyducky/src/pages/Profile.js
@@ -1,18 +1,44 @@
 import React from "react";
 import { Container, Card } from "react-bootstrap";
 
-const Profile = () => {
+function Profile() {
 	return (
-		<Container className="mt-4">
-			<Card>
-				<Card.Body>
-					<h3>User Profile</h3>
-					<p>Name: John Doe</p>
-					<p>Email: john.doe@example.com</p>
-				</Card.Body>
-			</Card>
-		</Container>
-	);
-};
+		<div className="background">
+			<div className="profile-side">
+				<h1>Profile</h1>
+
+
+			</div>
+
+			<div className="account-side">
+				<h2>Account</h2>
+
+				<label>Name</label>
+				<input type="text" placeholder="Annie"/>
+
+				<label>Year</label>
+				<select required>
+					<option disabled selected>1</option>
+					<option>2</option>
+					<option>3</option>
+					<option>4</option>
+				</select>
+
+				<label>Major</label>
+				<select required>
+					<option disabled selected>Business Analytics</option>
+					<option>Architecture</option>
+					<option>Business Administration</option>
+					<option>Business Artificial Intelligence Systems</option>
+					<option>Computer Science</option>
+				</select>
+
+				<label>Modules taken this semester</label>
+
+			</div>
+
+		</div>
+	)
+}
 
 export default Profile;
