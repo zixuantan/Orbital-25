ded20f3e4011d8f50ab04ad1bf2f59af5a3f7576
lorelier <joliengxuan@gmail.com>
2025-05-27 02:23:51 +0800

    added in images for profile and badges

--- studyducky/src/pages/Profile.js
+++ studyducky/src/pages/Profile.js
@@ -5,30 +5,46 @@ function Profile() {
 	return (
 		<div className="overall-container">
 			<div className="profile-side">
+				<h1>Profile</h1>
 				<div className="details">
-
-					<h1>Profile</h1>
-
 					<div className="prof">
 
 						<img src="profilepic.png" alt="profile" width="185" height="185"></img>
 
 						<p>Annie</p>
 					</div>
-					<label id="about">About</label>
+					<p id="about">About</p>
 					<p id="desc">Just a motivated duck trying to stay afloat in the sea of deadlines. 🐣
 					Lover of matcha, late-night cramming, and productive silence.</p>
 				</div>
 
+				<p id="badge">Badges</p>
+
 				<div className="badge-section"> 
-					<label id="badge">Badges</label>
+					
+
+					<div className="first">
+
+						<img src="firstbadge.png" alt="Just Getting Started" width="100" height="100"></img>
+						<p id="first-title">Just Getting Started</p>
+						<p id="first-desc">Join the Flock</p>
+
+					</div>
+
+					<div className="second">
+
+						<img src="lockbadge.png" alt="Lock It In" width="100" height="100"></img>
+						<p id="second-title">Lock It In</p>
+						<p id="second-desc">Study more than an hour consecutively</p>
+
+					</div>
 
 				</div>
 
 			</div>
 
 			<div className="account-side">
-
+				
 				<div className="form-container">
 					<h1>Account</h1>
 					
