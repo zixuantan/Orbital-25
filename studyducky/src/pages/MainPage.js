c9bb0d0c60b4dda79d61cceac806a6a2c33c46c4
lorelier <joliengxuan@gmail.com>
2025-05-28 01:43:02 +0800

    started general outline of page

---
+++ studyducky/src/pages/MainPage.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./MainPage.css";
+
+function MainPage() {
+    return (
+        <div className="overall-page">
+            <div className="stats">
+
+                <h1>Study Statistics</h1>
+
+            </div>
+
+            <div className="chats">
+                
+            </div>
+        </div>
+    )
+}
