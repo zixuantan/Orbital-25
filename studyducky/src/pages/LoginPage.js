e13c72df2e6c6b867ccfa904348a32c4d93809f6
lorelier <joliengxuan@gmail.com>
2025-05-31 02:30:03 +0800

    changed layout of login page left with button styling

--- studyducky/src/pages/LoginPage.js
+++ studyducky/src/pages/LoginPage.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import "./LoginPage.css";
+import './LoginPage.css';
 
 function LoginPage() {
 	return (
-		<div className="overall-page">
-			<div className="header">
+		<div className="login-page">
+			<div className="logo-header">
 
 				<img src="logo.png" alt="logo" width="150" height="150"/>
-				<h1>StudyDucky</h1>
+				<h1 id="website">StudyDucky</h1>
 
 			</div>
 
-			<p>Kickstart your study journey now!</p>
-			<button 
+			<p id="motiv">Kickstart your study journey now!</p>
+			<button className="login-btn" 
 				onClick={() =>
 					(window.location.href =
 						"http://localhost:5050/auth/google")
