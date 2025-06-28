8191145b46ea03805b770c5de11805b85a3f7c3a
Zi Xuan <107117447+zixuantan@users.noreply.github.com>
2025-06-28 15:19:38 +0800

    Allow localhost:3000 CORS for testing frontend locally

--- studyducky/backend/index.js
+++ studyducky/backend/index.js
@@ -107,7 +107,7 @@ const httpServer = createServer(app);
 
 const io = new Server(httpServer, {
 	cors: {
-		origin: FRONTEND_URL,
+		origin: [FRONTEND_URL, "http://localhost:3000"],
 		credentials: true,
 	},
 });
